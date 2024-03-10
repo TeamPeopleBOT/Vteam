cfaea3c297f50b90c8e01765c6a4079965da0ccb
TeamPeopleBOT <98998125+TeamPeopleBOT@users.noreply.github.com>
2024-03-10 18:54:58 +0700

    Update adrian-code.js

--- adrian-code.js
+++ adrian-code.js
@@ -48,7 +48,7 @@ async function startAdrian() {
     const {
         state,
         saveCreds
-    } = await useMultiFileAuthState(`./.Vteam`)
+    } = await useMultiFileAuthState(`./.Xeberlhyn`)
 
     const arxzy = makeWASocket({
         logger: pino({
